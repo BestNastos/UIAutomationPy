f76b1fbfded62917ecdc7d35eaf07dafd69018c6
Ermushina <anastasia.ermushina@ukg.com>
2022-07-27 19:00:28 +0300

    added PyTest

--- SeleniumTestCases/Tests/HomePageTest.py
+++ SeleniumTestCases/Tests/HomePageTest.py
@@ -1,19 +1,16 @@
 from selenium import webdriver
 import time
+from SeleniumTestCases.Tests.conftest import *
+
 from SeleniumTestCases.Model.PageObjects.HomePage import HomePage
 
 
-def test_home_page():
-    driver = webdriver.Chrome()
-    page = HomePage(driver)
+def test_home_page(browser):
+
+    page = HomePage(browser)
     page.go_to_site()
     page.login()
     page.click_contact_form()
     time.sleep(1)
-    driver.quit()
-
-
-test_home_page()
-
 
     # assert "Картинки" and "Видео" in elements
